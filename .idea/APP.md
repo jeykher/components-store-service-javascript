9f99f6ada28461bd1ed22b50599fac29fa332494
Jeykher <jeykheryendes@gmail.com>
2022-07-06 16:20:39 -0400

    06-07-2022_REP00001_FirstProjectCommit

---
+++ .idea/APP.md
@@ -0,0 +1,17 @@
+# Component Store Service JavaScript
+
+## Description
+
+This project contain storybook component javascript server
+
+## Bitbucket repository
+
+https://Jeykher@bitbucket.org/Jeykher/components-store-service-javascript.git
+
+## Gitlab repository
+
+
+
+## Github repository
+
+
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<h1>Component Store Service JavaScript</h1>
+	</React.StrictMode>
+);
+
+serviceWorkerRegistration.unregister();
+
+reportWebVitals();
